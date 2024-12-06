79bdbef2aa1cae780866d67fea0ae9b740990e67
Martijn-Kalkman <kalkmanwm@gmail.com>
2024-12-06 03:16:58 +0100

    last changes

--- pages/ontwikkelen.vue
+++ pages/ontwikkelen.vue
@@ -8,7 +8,95 @@ import Navbar from "./components/navbar.vue";
     <div class="mx-auto 2xl:w-6/12 p-4">
       <div class="">
         <h1 class="text-6xl font-bold text-center">Ontwikkelen</h1>
+        <h3 class="text-3xl mb-2 font-bold text-white">
+          - Technische kennis en analyse
+        </h3>
+        <h4 class="text-2xl">- Terugblik</h4>
+        <p class="pt-4">
+          Voordat ik begin met het bouwen van websites voor klanten, hou ik me
+          aan een vaste structuur. Zo plan ik altijd eerst een meeting met de
+          klant om te bespreken wat de klant wil en wat ze van mij kunnen
+          verwachten. Daarna ga ik aan de slag met het maken van een design.
+          Zodra dit design is goedgekeurd, kies ik het juiste framework dat
+          aansluit bij de behoeften van de klant. Meestal gebruik ik het
+          framework NUXT. Dit doe ik omdat ik hiermee een betere SEO-score kan
+          halen voor de websites die ik bouw.
+        </p>
+        <p class="pt-4">
+          Voordat ik begin met het bouwen van websites voor klanten, hou ik me
+          aan een vaste structuur. Zo plan ik altijd eerst een meeting met de
+          klant om te bespreken wat de klant wil en wat ze van mij kunnen
+          verwachten. Daarna ga ik aan de slag met het maken van een design.
+          Zodra dit design is goedgekeurd, kies ik het juiste framework dat
+          aansluit bij de behoeften van de klant. Meestal gebruik ik het
+          framework NUXT. Dit doe ik omdat ik hiermee een betere SEO-score kan
+          halen voor de websites die ik bouw.
+        </p>
+        <p class="pt-4">
+          Om mijn code herbruikbaar en begrijpelijk te maken, volg ik het advies
+          van mijn mentor op: ik gebruik geen comments meer in mijn code. Dit
+          doe ik zodat ik de code zelf duidelijk moet maken. Als ik bijvoorbeeld
+          een functie schrijf die gebruikers aan een database moet toevoegen,
+          noem ik deze functie simpel addUsers. Deze functie doet dan ook alleen
+          dat, zodat iedereen meteen begrijpt wat de functie doet.
+        </p>
+        <div class="flex lg:flex-row flex-col">
+          <div class="lg:w-6/12 p-4">
+            <img
+              class="mt-4 rounded-xl border-gray-800 transition-all"
+              src="/NUXTcode.png"
+              alt="Floating Image"
+            />
+            <p class="italic text-sm">Figure 1: Code van het framework Nuxt</p>
+          </div>
+          <div class="lg:w-6/12 p-4">
+            <img
+              class="mt-4 rounded-xl border-gray-800 transition-all"
+              src="/NUXTfunctie.png"
+              alt="Floating Image"
+            />
+            <p class="italic text-sm">
+              Figure 2: Functie met de juiste benaming
+            </p>
+          </div>
+        </div>
+        <p class="pt-4">
+          Doordat ik nu veel werk met het Nuxt-framework, krijg ik steeds meer
+          inzicht in hoe ik dit optimaal kan gebruiken. Mijn websites worden
+          niet alleen sneller, maar ook beter vindbaar in zoekmachines dankzij
+          verbeterde SEO.
+        </p>
+        <p class="pt-4">
+          Om de snelheid van de websites die ik bouw verder te verhogen, ben ik
+          ook bezig met het optimaliseren van andere onderdelen. Zo heb ik
+          bijvoorbeeld alle foto’s omgezet naar het .webp-formaat in plaats van
+          PNG of JPG, zodat de laadtijd van de website veel sneller wordt.
+        </p>
+        <p class="pt-4">
+          Omdat mijn partner en ik samen een bepaalde structuur aan het opbouwen
+          zijn voor hoe wij te werk gaan, wordt het makkelijker om elkaar te
+          helpen. Het is daarom erg belangrijk dat we binnen ons bedrijf
+          dezelfde structuur blijven gebruiken. Op deze manier kunnen we elkaars
+          code altijd snel begrijpen en aanpassen wanneer dat nodig is.
+        </p>
       </div>
+      <h4 class="text-2xl mt-4">- Vooruitzicht</h4>
+      <p>
+        In de toekomst wil ik de structuur van mijn werk binnen het bedrijf
+        verder uitbreiden. De technieken die ik nu gebruik wil ik blijven
+        gebruiken, maar ik wil er voor zorgen dat ik nog efficiënter te werk kan
+        gaan. Ook wil ik mijn kennis verder vergroten door andere frameworks te
+        onderzoeken en te vergelijken met wat ik nu gebruik. Het doel is om voor
+        elke klant de beste oplossing te vinden die volledig aansluit bij de
+        specifieke behoeften van hun project.
+      </p>
+      <h3 class="text-3xl mb-2 mt-4 font-bold text-white">
+        - Implementeren, testen en opleveren
+      </h3>
+      <h4 class="text-2xl">- Terugblik</h4>
+      <p></p>
+      <h4 class="text-2xl mt-4">- Vooruitzicht</h4>
+      <p></p>
     </div>
   </div>
 </template>
