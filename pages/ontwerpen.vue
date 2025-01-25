dcca7bc75fc6fc12a4e318d81ceb69d9edbb5db1
Martijn-Kalkman <kalkmanmartijn@gmail.com>
2025-01-25 15:38:38 +0100

    almost there

--- pages/ontwerpen.vue
+++ pages/ontwerpen.vue
@@ -27,60 +27,68 @@ import OpenModal from "./components/openModal.vue";
         <span class="text-3xl text-white font-bold">- Conceptualiseren: </span>
         <h4 class="text-2xl mt-2">- Terugblik</h4>
         <p class="mt-4">
-          Als ik een website moet bouwen voor een klant, begin ik altijd met
+          Als ik een website voor een klant moet bouwen, begin ik altijd met
           onderzoek. Zo heb ik bijvoorbeeld een website ontworpen voor een
-          schilder. Het eerste wat ik dan doe is online kijken naar andere
-          schilderswebsites om inspiratie op te doen en te bekijken wat normaal
-          is in deze branche. Daarna overleg ik met de klant om zijn specifieke
+          schilder. Het eerste wat ik doe, is online kijken naar andere
+          schilderswebsites om inspiratie op te doen en te zien wat normaal is
+          in deze branche. Daarna overleg ik met de klant om zijn specifieke
           wensen en behoeften te bespreken. Tijdens dit gesprek laat ik ook zien
           wat ik heb gevonden, zodat we samen kunnen bepalen wat het beste bij
           zijn bedrijf past.
         </p>
         <p class="mt-4">
-          De schilder waarvoor ik deze website heb gemaakt, wilde vooral laten
-          zien hoeveel ervaring hij heeft. Daarom heb ik een sectie ontworpen
-          waar hij eenvoudig zijn eerdere projecten kan laten zien. Uit mijn
-          onderzoek naar schilderswebsites zag ik dat veel van deze sites
-          gebruikmaken van foto's van schilders die aan het werk zijn.Daarom heb
-          ik ervoor gekozen om dit door de hele website terug te laten komen. Op
-          deze manier krijgt de website een professionele uitstraling die past
-          bij zijn doelgroep.
+          De schilder voor wie ik deze website heb gemaakt, wilde vooral zijn
+          ervaring benadrukken. Daarom heb ik een sectie ontworpen waarin hij
+          eenvoudig zijn eerdere projecten kan laten zien. Uit mijn onderzoek
+          naar andere schilderswebsites bleek dat veel van deze sites foto's
+          gebruiken van schilders die aan het werk zijn. Daarom heb ik ervoor
+          gekozen dit door de hele website terug te laten komen. Zo krijgt de
+          website een professionele uitstraling die goed past bij zijn
+          doelgroep.
         </p>
         <img
           class="mt-4 rounded-xl border-gray-800 transition-all"
           src="/figuur5.png"
-          alt="Floating Image"
+          alt="Figuur 5: Website design met een kwast als achtergrond"
         />
         <p class="italic text-sm">
           Figuur 5: Website design met een kwast als achtergrond
         </p>
-        <span class="font-bold">Crazy 8:</span>
         <p class="mt-4">
-          Om op meer ideeën te komen, maak ik vaak gebruik van de techniek Crazy
-          8. Hierbij probeer ik binnen een korte tijd acht verschillende
+          Link clickable wireframe desktop:
+          <a
+            class="text-blue-500"
+            href="        https://www.figma.com/proto/22BCEeSKZ8lTnOPhEnIncn/Schilder?node-id=1-2&p=f&t=QAW6IkNo6u5WSz8Y-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2
+          "
+            >Wireframe desktop</a
+          >
+        </p>
+        <h3 class="font-bold mt-4 text-2xl">Crazy 8:</h3>
+        <p class="mt-4">
+          Om meer ideeën te genereren, maak ik vaak gebruik van de techniek
+          Crazy 8. Hierbij probeer ik binnen korte tijd acht verschillende
           ontwerpen te bedenken en te schetsen. Dit helpt me om na te denken
-          over hoe ik het ontwerp eruit wil laten zien. Nadat ik acht schetsen
-          heb gemaakt, kies ik de drie beste uit. Deze werk ik vervolgens verder
-          uit in meer detail om te zien hoe deze ideeën er meer gedetailleerd
-          uitkomen.
+          over hoe het ontwerp eruit zou kunnen zien. Nadat ik acht schetsen heb
+          gemaakt, kies ik de drie beste uit. Deze werk ik vervolgens verder uit
+          in meer detail om te zien hoe ze er in de praktijk uitkomen.
         </p>
         <p class="mt-4">
           Aan het einde van dit proces heb ik een beter beeld van welk concept
           het beste werkt voor de website. Tijdens dit proces let ik niet alleen
-          op de designs, maar ook op de gebruikers van het eindproduct, en de
-          manier waarop het gebruikt kan worden. Dit idee werk ik daarna verder
-          uit in Figma, zodat ik een duidelijker wireframe kan maken. Via figma
-          kan ik de schetsen uitwerken en aan de klant laten zien.
+          op het ontwerp, maar ook op de gebruikers van het eindproduct en de
+          manier waarop het gebruikt zal worden. Dit idee werk ik daarna verder
+          uit in Figma, zodat ik een duidelijker wireframe kan maken. Via Figma
+          kan ik de schetsen verder uitwerken en aan de klant laten zien.
         </p>
         <p class="mt-4">
-          De designs die ik maak voor klanten zijn altijd afgestemd op hun
+          De ontwerpen die ik maak voor klanten zijn altijd afgestemd op hun
           specifieke behoeften en stijl. Zo heb ik bijvoorbeeld een website
           ontworpen voor een DJ, waarbij ik donkere kleuren heb gebruikt om aan
           te sluiten bij zijn branding. Dit pas ik toe bij elke klant. Meestal
-          begin ik met het kijken naar het logo om een passend kleurenpalet te
+          begin ik met het bekijken van het logo om een passend kleurenpalet te
           kiezen. Uiteraard bespreek ik dit eerst met de klant voordat ik begin
-          met het ontwerpen van een wireframe. Hierdoor zorg ik ervoor dat het
-          design niet alleen visueel aantrekkelijk is, maar ook volledig
+          met het ontwerpen van een wireframe. Op deze manier zorg ik ervoor dat
+          het ontwerp niet alleen visueel aantrekkelijk is, maar ook volledig
           aansluit bij de identiteit van de klant.
         </p>
         <div class="flex lg:flex-row flex-col">
@@ -109,72 +117,77 @@ import OpenModal from "./components/openModal.vue";
         <h4 class="text-2xl mt-2">- Vooruitzicht</h4>
         <p class="mt-4">
           In de toekomst wil ik meer onderzoek doen naar websites die relevant
-          zijn voor mijn klanten. Tot nu toe heb ik vooral websites bezocht die
-          te maken hebben met de branche van de klant. In de toekomst wil ik dit
-          uitbreiden door ook andere techieken toe te passen.
+          zijn voor mijn klanten. Tot nu toe heb ik voornamelijk websites
+          bekeken die te maken hebben met de branche van de klant. In de
+          toekomst wil ik dit uitbreiden door ook andere technieken toe te
+          passen.
         </p>
         <p class="mt-4">
-          Ook wil ik meer tijd besteden aan het analyseren van gebruikers, en
-          bekijken hoe bezoekers websites gebruiken. Op die manier kan ik nog
-          beter inschatten wat nodig is voor de klant, en welke verbeterd kunnen
-          worden. Dit wil ik meenemen in mijn toekomstige projecten, zodat ik
-          niet alleen mooie, maar ook functionele websites kan ontwerpen die
-          perfect aansluiten bij de wensen van mijn klanten.
+          Ook wil ik meer tijd besteden aan het analyseren van gebruikers en
+          kijken hoe bezoekers websites gebruiken. Op die manier kan ik beter
+          inschatten wat nodig is voor de klant en welke verbeteringen mogelijk
+          zijn. Dit wil ik meenemen in mijn toekomstige projecten, zodat ik niet
+          alleen mooie, maar ook functionele websites kan ontwerpen die perfect
+          aansluiten bij de wensen van mijn klanten.
         </p>
         <p class="text-3xl mt-4 text-white font-bold">
           - Ontwerpen en prototypen:
         </p>
         <h4 class="text-2xl mt-2">- Terugblik</h4>
         <p class="mt-4">
-          Voordat ik begin met het maken van een design moet ik eerst altijd
-          nadenken over de functionaliteit die nodig is. Dit doe ik doormiddel
-          van User stories. User stories voor de website van HI-LO(DJ)
+          Hier is de verbeterde versie van je tekst: Voordat ik begin met het
+          maken van een design, moet ik eerst altijd nadenken over de
+          functionaliteit die nodig is. Dit doe ik door middel van user stories.
+          Hier zijn enkele user stories voor de website van HI-LO (DJ):
         </p>
         <ul class="mt-4">
-          <li>- Als eigenaar wil ik events op de website kunnen zetten</li>
+          <li>- Als eigenaar wil ik events op de website kunnen plaatsen.</li>
           <li>
-            - Als eigenaar wil ik de laatste muziek laten zien op de website
+            - Als eigenaar wil ik de laatste muziek die de DJ speelt kunnen
+            tonen op de website.
           </li>
-          <li>- Als eigenaar wil ik laten zien wat voor merch er te koop is</li>
-          <li>- Als gebruiker wil ik muziek kunnen luister van de dj</li>
-          <li>- Als gebruiker wil ik snel zien waar deze dj optreed</li>
           <li>
-            - Als gebruiker wil ik makkelijk kunnen navigeren door de website
+            - Als eigenaar wil ik de beschikbare merch op de website kunnen
+            weergeven.
+          </li>
+          <li>- Als gebruiker wil ik muziek van de DJ kunnen beluisteren.</li>
+          <li>- Als gebruiker wil ik snel kunnen zien waar de DJ optreedt.</li>
+          <li>
+            - Als gebruiker wil ik makkelijk kunnen navigeren door de website.
           </li>
         </ul>
         <p class="mt-4">
-          Door deze user stories weet ik precies wat er in de website moet
-          komen. Vanuit deze informatie begin ik met het maken van een
+          Door deze user stories weet ik precies wat er op de website moet
+          komen. Op basis van deze informatie begin ik met het maken van een
           interactieontwerp in de vorm van wireframes.
         </p>
         <p class="mt-4">
           Om te controleren of mijn wireframes goed zijn uitgewerkt, schrijf ik
           voor mezelf een aantal taken op die ik op de website moet kunnen
           uitvoeren. Deze taken voer ik vervolgens zelf uit om te testen hoe
-          gebruiksvriendelijk het design is. Als ik merk dat ik een te grote
-          omweg moet maken om een taak uit te voeren, pas ik het design aan. Zo
+          gebruiksvriendelijk het ontwerp is. Als ik merk dat ik een te grote
+          omweg moet maken om een taak uit te voeren, pas ik het ontwerp aan. Zo
           zorg ik ervoor dat de user stories volledig overeenkomen met het
-          design en dat zowel de gebruikers als de eigenaar van de website
+          ontwerp en dat zowel de gebruikers als de eigenaar van de website
           tevreden zijn.
         </p>
         <h4 class="text-2xl mt-2">- Vooruitzicht</h4>
         <p class="mt-4">
           In de toekomst wil ik mijn ontwerpen verder verbeteren door meer
-          gebruikerstesten te doen, en de feedback van gebruikers mee te nemen.
-          Dit helpt om de gebruiksvriendelijkheid van mijn websites nog beter te
-          maken.
+          gebruikerstests uit te voeren en de feedback van gebruikers mee te
+          nemen. Dit helpt om de gebruiksvriendelijkheid van mijn websites nog
+          verder te verbeteren.
         </p>
         <p class="mt-4">
           In de toekomst wil ik mijn aanpak met user stories verbeteren. Ik wil
           meer tijd besteden aan het maken en onderzoeken van user stories,
-          zodat ik nog beter weet wat de behoeften zijn van de eigenaar en de
-          gebruikers van de website. Ook wil ik meer testen uitvoeren door
-          gebruikerstesten in te plannen met andere mensen, zodat ik meer
+          zodat ik nog beter begrijp wat de behoeften zijn van zowel de eigenaar
+          als de gebruikers van de website. Ook wil ik meer testen uitvoeren
+          door gebruikerstests in te plannen met andere mensen, zodat ik meer
           feedback krijg over de gebruiksvriendelijkheid van mijn wireframes. Op
-          deze manier kan ik mijn designs nog beter aanpassen op de gebruikers
-          van de website.
+          deze manier kan ik mijn ontwerpen beter afstemmen op de gebruikers van
+          de website.
         </p>
-
       </div>
     </div>
   </div>
