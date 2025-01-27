a76d1b211097b133c69464c1fd05487a97afb27b
Martijn-Kalkman <kalkmanmartijn@gmail.com>
2025-01-27 16:58:33 +0100

    progress

--- pages/leafylines.vue
+++ pages/leafylines.vue
@@ -19,84 +19,133 @@ import Navbar from "./components/navbar.vue";
           is een klein bedrijf dat zich wil richten op het bouwen van websites
           voor business to business. Ik wil de klanten van mijn bedrijf zo goed
           mogelijk helpen, en zelfs een stapje verder gaan om er voor te zorgen
-          dat <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          een goede naam krijgt. Zo ben ik nu erg gefocused met
-          het opbouwen van een goed portfolio om wat vertrouwelijker online te
-          worden.
+          dat
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          een goede naam krijgt. Zo ben ik nu erg gefocused met het opbouwen van
+          een goed portfolio om wat vertrouwelijker online te worden.
         </p>
         <p class="mt-4">
-          De naam <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          is een combinatie van de woorden "leaf" en "lines".
-          Zo staat Leaf voor clean code en willen we onze code duurzaam en
-          schoon houden, en Lines staat voor de lijnen die we maken in de code
+          De naam
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          is een combinatie van de woorden "leaf" en "lines". Zo staat Leaf voor
+          clean code en willen we onze code duurzaam en schoon houden, en Lines
+          staat voor de lijnen die we maken in de code
         </p>
-        <h3 class="mt-4 font-bold text-2xl">Waarom kiezen voor <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-            ?</h3>
+        <h3 class="mt-4 font-bold text-2xl">
+          Waarom kiezen voor
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          ?
+        </h3>
 
         <p class="mt-4">
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          krijg je geen standaardoplossing, maar een website die
-          echt bij jou en je bedrijf past. Of je nu net begint of al een
-          gevestigde naam hebt, wij zorgen voor een oplossing die jouw visie en
-          doelen weerspiegelt. We combineren technische kennis met creatief
-          ontwerp, zodat je niet alleen een mooie, maar ook een
-          gebruiksvriendelijke en efficiënte website krijgt. Ons doel is om je
-          online succes te vergroten, met een website die niet alleen goed
-          presteert, maar die ook de juiste indruk maakt bij je klanten.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          krijg je geen standaardoplossing, maar een website die echt bij jou en
+          je bedrijf past. Of je nu net begint of al een gevestigde naam hebt,
+          wij zorgen voor een oplossing die jouw visie en doelen weerspiegelt.
+          We combineren technische kennis met creatief ontwerp, zodat je niet
+          alleen een mooie, maar ook een gebruiksvriendelijke en efficiënte
+          website krijgt. Ons doel is om je online succes te vergroten, met een
+          website die niet alleen goed presteert, maar die ook de juiste indruk
+          maakt bij je klanten.
         </p>
         <h3 class="mt-4 font-bold text-2xl">Consultancy</h3>
         <p>
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          bieden we deskundig advies om het meeste uit je online
-          aanwezigheid te halen. Of je nu begeleiding nodig hebt op het gebied
-          van strategie, design of technologie, wij bieden duidelijke
-          aanbevelingen en praktische oplossingen. We werken samen met jou om je
-          doelen te begrijpen en een plan te creëren dat succes stimuleert.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          bieden we deskundig advies om het meeste uit je online aanwezigheid te
+          halen. Of je nu begeleiding nodig hebt op het gebied van strategie,
+          design of technologie, wij bieden duidelijke aanbevelingen en
+          praktische oplossingen. We werken samen met jou om je doelen te
+          begrijpen en een plan te creëren dat succes stimuleert.
         </p>
         <h3 class="mt-4 font-bold text-2xl">Ontwikkeling</h3>
         <p>
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          creëren we websites en webapplicaties die goed werken
-          en er geweldig uitzien. Wij gebruiken moderne tools om snelle, veilige
-          websites te bouwen die meegroeien met je bedrijf. We focussen ons op
-          het zorgen dat alles, van de backend tot de frontend, soepel draait,
-          zodat je gebruikers een geweldige ervaring hebben.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          creëren we websites en webapplicaties die goed werken en er geweldig
+          uitzien. Wij gebruiken moderne tools om snelle, veilige websites te
+          bouwen die meegroeien met je bedrijf. We focussen ons op het zorgen
+          dat alles, van de backend tot de frontend, soepel draait, zodat je
+          gebruikers een geweldige ervaring hebben.
         </p>
         <h3 class="mt-4 font-bold text-2xl">SEO</h3>
         <p>
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          richten we ons op het verbeteren van de zichtbaarheid
-          van je website in zoekmachines. Onze SEO-strategieën helpen meer
-          verkeer naar je site te trekken, waardoor het gemakkelijker wordt voor
-          potentiële klanten om je te vinden. We passen onze aanpak aan op jouw
-          bedrijf en vergroten effectief je online aanwezigheid.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          richten we ons op het verbeteren van de zichtbaarheid van je website
+          in zoekmachines. Onze SEO-strategieën helpen meer verkeer naar je site
+          te trekken, waardoor het gemakkelijker wordt voor potentiële klanten
+          om je te vinden. We passen onze aanpak aan op jouw bedrijf en
+          vergroten effectief je online aanwezigheid.
         </p>
         <h3 class="mt-4 font-bold text-2xl">Ontwerpen</h3>
         <p>
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          creëren we mooie, gebruiksvriendelijke websiteontwerpen
-          die passen bij het merk van de klant. Ons doel is om je site er
-          geweldig uit te laten zien, terwijl we zorgen voor een soepele
-          ervaring voor je bezoekers. We ontwerpen met de behoeften van jouw
-          bedrijf in gedachten, van begin tot eind.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          creëren we mooie, gebruiksvriendelijke websiteontwerpen die passen bij
+          het merk van de klant. Ons doel is om je site er geweldig uit te laten
+          zien, terwijl we zorgen voor een soepele ervaring voor je bezoekers.
+          We ontwerpen met de behoeften van jouw bedrijf in gedachten, van begin
+          tot eind.
         </p>
         <h3 class="mt-4 font-bold text-2xl">Hosting</h3>
         <p>
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          bieden we betrouwbare en veilige webhosting om ervoor
-          te zorgen dat je site soepel blijft draaien. Onze hostingdiensten
-          garanderen dat je website altijd toegankelijk, snel en beschermd is.
-          We bieden ondersteuning om eventuele problemen op te lossen en zorgen
-          ervoor dat je site zonder onderbrekingen blijft functioneren.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          bieden we betrouwbare en veilige webhosting om ervoor te zorgen dat je
+          site soepel blijft draaien. Onze hostingdiensten garanderen dat je
+          website altijd toegankelijk, snel en beschermd is. We bieden
+          ondersteuning om eventuele problemen op te lossen en zorgen ervoor dat
+          je site zonder onderbrekingen blijft functioneren.
         </p>
         <h3 class="mt-4 font-bold text-2xl">Onderhoud</h3>
         <p>
-          Bij <a class="text-blue-500" href="https://www.leafylines.com/">LeafyLines</a>
-          bieden we doorlopend onderhoud om je website in topvorm
-          te houden. We zorgen voor updates, beveiligingscontroles en
-          oplossingen om ervoor te zorgen dat je site functioneel en up-to-date
-          blijft. We staan klaar om eventuele problemen snel aan te pakken en
-          zorgen ervoor dat je website soepel blijft draaien.
+          Bij
+          <a class="text-blue-500" href="https://www.leafylines.com/"
+            >LeafyLines</a
+          >
+          bieden we doorlopend onderhoud om je website in topvorm te houden. We
+          zorgen voor updates, beveiligingscontroles en oplossingen om ervoor te
+          zorgen dat je site functioneel en up-to-date blijft. We staan klaar om
+          eventuele problemen snel aan te pakken en zorgen ervoor dat je website
+          soepel blijft draaien.
+        </p>
+        <h3 class="mt-4 font-bold text-3xl">De toekomst van LeafyLines</h3>
+        <p class="mt-4">
+          Bij LeafyLines kijken we altijd vooruit. We willen groeien als bedrijf
+          door meer klanten te helpen en onze kennis en vaardigheden verder te
+          ontwikkelen. Ons doel is om een betrouwbare partner te blijven voor
+          bedrijven die op zoek zijn naar goede websites en webapplicaties.
+        </p>
+        <p class="mt-4">
+          In de toekomst willen we ons meer richten op nieuwe technologieën. We
+          willen websites en applicaties maken die mooi en makkelijk te
+          gebruiken zijn, en ook goed meegroeien met het bedrijf van onze
+          klanten. Daarnaast blijven we ons inzetten om onze klanten beter
+          vindbaar te maken online.
+        </p>
+        <p class="mt-4">
+          Met oog voor kwaliteit, details en klantgericht werken, willen we
+          LeafyLines klaar maken voor een succesvolle toekomst.
         </p>
       </div>
     </div>
